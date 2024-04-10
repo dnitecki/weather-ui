0e52abf21157557faedbc32122394ba2695eeeb4
dnitecki <dominick.nitecki@outlook.com>
2024-04-09 22:41:54 -0400

    update

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -4,7 +4,6 @@ module.exports = {
   mode: "production",
   devtool: "source-map",
   plugins: [
-    new webpack.HotModuleReplacementPlugin(),
     // Define new env variables
     new webpack.DefinePlugin({
       "process.env.name": JSON.stringify("production"),
